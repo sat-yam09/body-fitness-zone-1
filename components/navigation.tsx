9acd3c43288d5e326a7ee436627af22c5fbfefc4
Satyam Choudhary <satyamchoudharyfreefree@gmail.com>
2025-09-07 23:03:29 +0530

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -7,7 +7,6 @@ import { Menu, X } from "lucide-react"
 const navItems = [
   { name: "HOME", href: "/" },
   { name: "TRAINING", href: "/training" },
-  { name: "TOUR", href: "/tour" },
   { name: "CONTACT", href: "/contact" },
 ]
 
